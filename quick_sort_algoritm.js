0b1f6b601de6f3fed99875499d7fc4e007dc0bee
Mahmoud Alwakeel <alwakeel@Mahmouds-MacBook-Pro.local>
2023-11-26 14:53:55 +0200

    quick sort algo

---
+++ quick_sort_algoritm.js
@@ -0,0 +1,18 @@
+function quickSort(array) {
+    if (array.length < 2) {
+        return array
+    } else {
+        let pivot = array[0]
+        let smaller = []
+        let larger = []
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] < pivot) {
+                smaller.push(array[i])
+            } else {
+                larger.push(array[i])
+            }
+        }
+    return quickSort(smaller).concat([pivot], quickSort(larger))
+    }
+}
+console.log(quickSort([10, 12, 1, 9, 11]))
